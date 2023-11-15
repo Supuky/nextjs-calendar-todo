3dc6640c2c4162a15c6fa094c73dfc9ce7f86f5b
Supuky <saito.hajime0112@gmail.com>
2023-11-15 21:20:42 +0900

    全体のpadding調整

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Todos from "./ui/todos/todos";
 
 export default function Page() {
   return (
-    <main className="min-h-screen  p-4">
+    <main className="min-h-screen p-1 sm:p-4">
       <nav className="flex justify-between mb-4 border-b border-violet-100 p-4">
         <h1 className="font-bold sm:text-2xl text-gray-700">Advanced Todo List</h1>
       </nav>
